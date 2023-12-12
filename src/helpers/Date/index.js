03b42cc93d4a96b37c329dcb065e2dd0a711eab4
Sylvain <sylvain.hutt@epitech.eu>
2023-12-12 11:41:34 +0100

    Push soutenance

---
+++ src/helpers/Date/index.js
@@ -0,0 +1,19 @@
+export const MONTHS = {
+  1: "janvier",
+  2: "février",
+  3: "mars",
+  4: "avril",
+  5: "mai",
+  6: "juin",
+  7: "juillet",
+  8: "août",
+  9: "septembre",
+  10: "octobre",
+  11: "novembre",
+  12: "décembre",
+};
+
+export const getMonth = (date) => {
+  const month = MONTHS[date.getMonth() + 1];
+  return month;
+};
